3f6e056895d21511ae855b1500e9148a0eb655fd
Nemanja <nemadjo94@gmail.com>
2019-06-11 14:11:54 +0200

    Update Settings.js

--- components/Settings.js
+++ components/Settings.js
@@ -1,6 +1,3 @@
-/* eslint-disable react/prefer-stateless-function */
-/* eslint-disable linebreak-style */
-/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { View, Text } from 'react-native';
 

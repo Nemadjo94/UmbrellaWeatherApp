6ebd77c2c0c95de9c4056601d214215254c81754
Nemanja <nemadjo94@gmail.com>
2019-06-11 14:27:02 +0200

    Update apiKeys.js

--- apiKeys.js
+++ apiKeys.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
+//Get your own keys!!!
 export default apiKeys = {
-  ipstack: '34d3a5f5ac759b61be28b1c35e9092ef',
-  googleSearch: 'AIzaSyCrph1i4fC7S45fctP0Shsp72DWqT145Ww',
-  googleCx: '002278705843934428716:arkupjibqng',
-  openWeather: 'dc45874b05239b0dd78000d5ded088da'
+  ipstack: '',
+  googleSearch: '',
+  googleCx: '',
+  openWeather: ''
 }
